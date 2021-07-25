3a785ae0774b7e1c42554ab87dd6fb3ef49b3bea
tony <antolisah@gmail.com>
2021-07-26 01:45:38 +0300

    added update links and delete link mutation to linkQueries

--- functions/utils/linkQueries.js
+++ functions/utils/linkQueries.js
@@ -31,7 +31,37 @@ const CREATE_LINK_MUTATION = `
   }
 `;
 
+const UPDATE_LINK_MUTATION = `
+  mutation($id: ID!, $archived: Boolean!, $name: String!, $url: String!, $description: String!) {
+    updateLink(
+      id: $id,
+      data: {
+        name: $name, 
+        url: $url, 
+        description: $description, 
+        archived: $archived
+      }
+    ) {
+      _id
+      name
+      url
+      description
+      archived
+    }
+  }
+`;
+
+const DELETE_LINK_MUTATION = `
+  mutation($id: ID!) {
+    deleteLink(id: $id){
+      _id
+    }
+  }
+`;
+
 module.exports = {
   GET_LINKS_QUERY,
   CREATE_LINK_MUTATION,
+  UPDATE_LINK_MUTATION,
+  DELETE_LINK_MUTATION,
 };
